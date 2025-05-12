b9c3372a1f720a2bc7e69d24424ea2750be32291
Duaa Ehtisham <dua.ehtisham69@gmail.com>
2025-05-12 12:02:13 +0500

    nihao fine shyt

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
+// Replace with your IP-based backend API
 export const API = axios.create({
-  baseURL: 'http://16.170.201.54:5000/',
-  withCredentials: true, 
+  baseURL: 'http://16.16.187.156:5000/',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-
 API.interceptors.request.use((config) => {
   return config;
 });
-
-
