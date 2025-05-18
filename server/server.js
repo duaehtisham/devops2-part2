6b432d989d351b88effc2c18dd78bc7e3f7e967c
duaehtisham <160533839+duaehtisham@users.noreply.github.com>
2025-05-19 00:56:07 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import shopRouter from "./routes/shops.js";
 dotenv.config();
 
 const app = express();
-//testinnnnnnnnn again
+//hellooooo again and again
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -39,8 +39,10 @@ if (!CONNECTION_URL) {
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, '0.0.0.0', () => {
+    app.listen(PORT, () => {
       console.log(`Server Running on port ${PORT}`);
     })
   )
   .catch((error) => console.error("Database connection error:", error.message));
+
+
